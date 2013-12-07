ef3391428f1983ad7fbe39cfc45606004dfcad24
Matthew McGarvey <mcgarveymr@gmail.com>
2013-12-07 11:01:16 -0500

    Initial commit.
    Argument checking has been implemented. Created the skeleton for the
    functions to be used.

---
+++ startbrewing.c
@@ -0,0 +1,32 @@
+#include <bcm2835.h>
+#include <cjson.h>
+#include <curl/curl.h>
+#include <oauth.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		printf("usage: startbrewing file");
+	} else {
+		read_in_keys();
+	}
+	
+
+	return 0;
+}
+
+void read_in_keys() {
+	
+}
+
+void stream_init()
+{
+	
+}
+
+void parse()
+{
+	
+}
+
