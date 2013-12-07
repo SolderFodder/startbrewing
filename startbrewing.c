3b20170cc2c66fe8ccad23d22c980bb90eaaea0c
Matthew McGarvey <mcgarveymr@gmail.com>
2013-12-07 12:34:30 -0500

    Working on reading in the oauth key text file.
    Created a struct to hold oauth key data. Will then read in keys from a
    separate file. Still implementing that functionality, but the roommates
    are demanding that we make waffles now so I want to commit this and
    save the work.

--- startbrewing.c
+++ startbrewing.c
@@ -3,21 +3,40 @@
 #include <curl/curl.h>
 #include <oauth.h>
 
+struct oauth_keys {
+	char *ckey;
+	char *csecret;
+	char *atok;
+	char *atoksecret;
+};
+
 int main(int argc, char *argv[])
 {
+	oauth_keys *keys;
+	
 	if(argc != 2)
 	{
 		printf("usage: startbrewing file");
 	} else {
-		read_in_keys();
+		read_in_keys(argv[1]);
 	}
 	
 
 	return 0;
 }
 
-void read_in_keys() {
+void read_in_keys(char *file_name) {
+	char *buffer;
+	long size;
+	size_t result;
+	FILE *file = fopen(file_name, "r");
 	
+	fseek()
+
+	buffer = (char*)malloc(sizeof(char)*lsize);
+
+	fread(keys, 1, sizeof(keys))
+
 }
 
 void stream_init()
