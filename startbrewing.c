941a05c1671057cbef82bb6f42e137b7400a04f8
Matthew McGarvey <mcgarveymr@gmail.com>
2013-12-07 21:18:01 -0500

    Can open key file, but not reading properly yet.
    Trying to get the first line of the key file to be properly read, but
    only the last three lines are being read correctly. The first line
    appears to be a combination of all four lines. It doesn't matter if the
    file lines are being read with fgets or fscanf.

--- startbrewing.c
+++ startbrewing.c
@@ -1,7 +1,10 @@
-#include <bcm2835.h>
-#include <cjson.h>
-#include <curl/curl.h>
-#include <oauth.h>
+//#include <bcm2835.h>
+//#include <cjson.h>
+//#include <curl/curl.h>
+//#include <oauth.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 struct oauth_keys {
 	char *ckey;
@@ -10,33 +13,45 @@ struct oauth_keys {
 	char *atoksecret;
 };
 
-int main(int argc, char *argv[])
+void read_in_keys(char*, struct oauth_keys*);
+
+int main(int argc, const char* argv[])
 {
-	oauth_keys *keys;
+	struct oauth_keys *keys = malloc(sizeof(struct oauth_keys)+1);
+	keys->ckey = malloc(sizeof(char*)+1);
+	keys->csecret = malloc(sizeof(char*)+1);
+	keys->atok = malloc(sizeof(char*)+1);
+	keys->atoksecret = malloc(sizeof(char*)+1);
+	char *filename = malloc(strlen(argv[1])+1);
+	
+	strcpy(filename, argv[1]);
 	
 	if(argc != 2)
 	{
 		printf("usage: startbrewing file");
 	} else {
-		read_in_keys(argv[1]);
+		read_in_keys(filename, keys);
+		
+		printf("%s\n", keys->ckey);
+		//printf("%s\n", keys->csecret);
+		//printf("%s\n", keys->atok);
+		//printf("%s\n", keys->atoksecret);
 	}
 	
 
 	return 0;
 }
 
-void read_in_keys(char *file_name) {
-	char *buffer;
-	long size;
-	size_t result;
-	FILE *file = fopen(file_name, "r");
-	
-	fseek()
+void read_in_keys(char *f, struct oauth_keys *k) {
 
-	buffer = (char*)malloc(sizeof(char)*lsize);
+	FILE *file = fopen(f, "r");
 
-	fread(keys, 1, sizeof(keys))
+	fscanf(file, "%s\n", k->ckey);
+	//fscanf(file, "%s\n", k->csecret);
+	//fgets(k->atok, 100, file);
+	//fgets(k->atoksecret, 100, file);
 
+	fclose(file);
 }
 
 void stream_init()
